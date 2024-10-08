14db203244804e006da9a17c41420b11c8619d4e
Martin Crepaz <e11776187@student.tuwien.ac.at>
2024-10-08 23:00:43 +0200

    try pipeline scripts

---
+++ examples/python/pipelines/visualize_point_cloud.py
@@ -0,0 +1,13 @@
+import open3d as o3d
+import numpy as np
+import copy
+
+
+print("Load a ply point cloud, print it, and render it")
+pcd = o3d.io.read_point_cloud("/home/martin/Desktop/Open3D/examples/python/pipelines/output0.ply")
+print(pcd)
+print(np.asarray(pcd.points))
+o3d.visualization.draw_geometries([pcd], zoom=0.3412,
+                                  front=[0.4257, -0.2125, -0.8795],
+                                  lookat=[2.6172, 2.0475, 1.532],
+                                  up=[-0.0694, -0.9768, 0.2024])
